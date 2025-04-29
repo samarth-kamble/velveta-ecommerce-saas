0613fff97e1c8c66ec52b36c5228b19355c2e933
Samarth Kamble <kamblesamarth04@gmail.com>
2025-04-29 10:36:10 +0530

    Intilizing a project and setup sever

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import axios from "axios";
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+
+app.use(
+  express.json({
+    limit: "100mb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    limit: "100mb",
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.set("trust proxy", 1);
+
+// Apply a Rate Limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100), // limit each IP to 1000 requests per windowMs
+  message: "Too many requests, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req: any) => req.ip,
+});
+
+app.use(limiter);
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
+});
+
+app.use("/", proxy("http://localhost:6001"));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on("error", console.error);
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,25 @@
-import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello Sam" });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.error("Server error:", err);
 });
